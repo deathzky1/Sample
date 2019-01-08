75a374420e3b7bb89182d28073a8da53a2d6731a
deathzky1 <khern02@yahoo.com>
2019-01-08 13:49:12 +0800

    Adding jFrame

---
+++ BasicElement.java
@@ -0,0 +1,11 @@
+class BasicElement{
+	
+	public static void main(String[] args){
+		Jframe myframe = new Jframe();
+
+		myFrame.setDefaultCloseOperation(myFrame.EXIT.ON.CLOSE);
+		frame.setVisible(true);
+	System.out.println("hello");
+
+	}
+}
